b143124fa9ad3b3b3b5e25c4b15990d82435f6cb
Valdir Lima <valdirlima508@gmail.com>
2022-03-23 10:13:36 -0400

    finished assignment

--- Testing/Models/Product.cs
+++ Testing/Models/Product.cs
@@ -1,4 +1,9 @@
-﻿namespace Testing.Models
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Testing.Models
 {
     public class Product
     {
@@ -8,6 +13,7 @@
         public int CategoryID { get; set; }
         public int OnSale { get; set; }
         public int StockLevel { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
     }
 
     
